195785a594c1874ab282c6cda538f13951ac8316
astipkovits <astipkovits@aviatrix.com>
2022-08-01 16:48:35 +0200

    Fixed outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "linux_mgmt_ip" {
-  value = azurerm_public_ip.linux_vm_pubip.ip_address
+  value = azurerm_public_ip.linux_vm_pubip[0].ip_address
 }
 
 output "linux_private_ip" {
-  value = azurerm_network_interface.linux_vm_iface.private_ip_address
+  value = azurerm_network_interface.linux_vm_iface[0].private_ip_address
 }
