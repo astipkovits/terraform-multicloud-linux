070d2dbefad1196099d696da7f2cd0916d56f087
astipkovits <astipkovits@aviatrix.com>
2022-08-30 23:16:56 +0200

    Added indexing to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "linux_mgmt_ip" {
-  value = var.cloud == "azure" ? azurerm_public_ip.linux_vm_pubip[0].ip_address : var.cloud == "aws" ? module.ec2_instance.public_ip : null
+  value = var.cloud == "azure" ? azurerm_public_ip.linux_vm_pubip[0].ip_address : var.cloud == "aws" ? module.ec2_instance[0].public_ip : null
 }
 
 output "linux_private_ip" {
-  value = var.cloud == "azure" ? azurerm_network_interface.linux_vm_iface[0].private_ip_address : var.cloud == "aws" ? module.ec2_instance.private_ip : null
+  value = var.cloud == "azure" ? azurerm_network_interface.linux_vm_iface[0].private_ip_address : var.cloud == "aws" ? module.ec2_instance[0].private_ip : null
 }
