162bdbcb6dd35c586c47ff6127c05c702daa7142
astipkovits <90217204+astipkovits@users.noreply.github.com>
2022-09-01 23:34:12 +0200

    Added NSG to the interface.

--- azure_ubuntu.tf
+++ azure_ubuntu.tf
@@ -25,6 +25,66 @@ resource "azurerm_network_interface" "linux_vm_iface" {
   }
 }
 
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "${var.name}-nsg"
+  location            = var.region
+  resource_group_name = var.azure_resource_group
+
+  security_rule {
+    name                       = "inbound-ssh"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "inbound-icmp"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Icmp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "inbound-http"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "outbound-any"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.linux_vm_iface[count.index].id
+  network_security_group_id = azurerm_network_security_group.nsg1
+}
+
+
 resource "azurerm_linux_virtual_machine" "linux_vm" {
   count = var.cloud == "azure" ? 1 :0
   name                = "${var.name}-vm"
